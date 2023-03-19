a63195a25b4f2aec3ca85c93f6f5321dc31fd2d9
DennisMukirai <dennismukirai17@gmail.com>
2023-03-18 21:40:05 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * description - using the main function
  * this program prints programming is like building a multilingual puzzle
- * return:0
+ * Return: Always 0 (success)
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
