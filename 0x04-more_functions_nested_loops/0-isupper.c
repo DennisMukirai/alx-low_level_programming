7d4c1928b74a28453549f8e6fbf2ae30fb0aad0c
DennisMukirai <dennismukirai17@gmail.com>
2023-03-24 00:00:41 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * isupper - checks for uppercase character
- * return: 1 if uppercase 0 if not
+ * _isupper - checks for uppercase character
+ *
+ * @c: the character to be checked
+ * Return: 1 if is uppercase, 0 if otherwise
  */
 
-int isupper(int c)
+int _isupper(int c)
 {
-	if (c => 65 && c <= 90)
-	{
-		putchar('c');
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
-		putchar('c');
 		return (0);
-	}
-	putchar('\n');
 }
