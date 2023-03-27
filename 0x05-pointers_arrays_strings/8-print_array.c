b16d4aded0960e4f42d32ad14618501eaf185f24
DennisMukirai <dennismukirai17@gmail.com>
2023-03-27 07:35:13 -0700

    committed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - Prints n elements of array
+*@a: pointer
+*@n: integer
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", *(a + i));
+		i++;
+
+		if (i < n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
