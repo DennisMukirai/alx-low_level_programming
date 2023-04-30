1cd7cdde179159c1a32fe711f72bee7cabdace9b
DennisMukirai <dennismukirai17@gmail.com>
2023-04-30 01:05:58 -0700

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,7 @@ int _strlen(const char *s)
   *Return: Address of the new element
   */
 
-list_t *add_node(list_t **head, const char *s)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
@@ -32,7 +32,7 @@ list_t *add_node(list_t **head, const char *s)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	new_node->len = strlen(str);
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (new_node);
